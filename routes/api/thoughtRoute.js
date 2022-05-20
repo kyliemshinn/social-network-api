8e6cd12ea1363d20dfb3e2df1fdf728f83f433db
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-19 17:39:45 -0700

    create index routes, add outline for user routes and thought routes

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,30 @@
+//      api/ thoughts
+
+    //GET all thoughts
+
+
+    //GET a single thought by ID
+
+
+    //POST to create a new thought
+
+
+    // PUT to update thought by ID
+
+
+    // DELETE thought by ID
+
+
+
+    
+
+//      /api/thoughts/:thoughtID/reactions
+
+
+
+    //POST to create a reaction
+
+    
+    //DELETE a reaction by reactions reactionID value
+
+    
---
+++ routes/api/userRoute.js
@@ -0,0 +1,21 @@
+
+    //  api/user
+    
+    //GET all users
+
+
+    //GET a single user by ID
+
+
+    //POST a new user
+
+
+
+    
+
+
+    //BONUS
+
+    // POST to add a new friend to user friend list
+
+    //DELETE to remove friend
