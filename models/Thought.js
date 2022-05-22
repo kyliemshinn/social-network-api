c37219f0cd70d6f767d80a5bcbbd3943646a67ff
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-22 15:47:37 -0700

    add more to thoughts model file to have full functioning

--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,70 @@
+const dateFormat = require("../utils/dateFormat");
+const { Schema, model, Types } = require("mongoose");
 
 
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: {type: String, required: true, minlength: 1, maxlength: 280},
-    createdAt: {type: Date, default: Date.now, /*use getter method to format the time stamp on query */},
-    username: { type: String, required: true}
-    
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        //add getter value
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
 
-})
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // add getter value
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+//create virtual property
+thoughtSchema.virtual("reactionCount").get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
