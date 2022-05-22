1729e89ca18ccb8f18e7be0ce0a4dbe6f67dc361
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-22 16:23:29 -0700

    add thougth routes to folder

---
+++ controllers/thoughtController.js
@@ -0,0 +1,20 @@
+//      api/ thoughts
+
+//     //GET all thoughts
+//     //POST to create a new thought
+//     router.route("/").get(getThoughts).post(createThought);
+
+//     //GET a single thought by ID
+//     // PUT to update thought by ID
+//     // DELETE thought by ID
+//     router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
+    
+
+// //      /api/thoughts/:thoughtID/reactions
+
+
+//     //POST to create a reaction
+//     //DELETE a reaction by reactions reactionID value
+// router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
+
+// module.exports = router;
--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,30 +1,20 @@
 //      api/ thoughts
 
     //GET all thoughts
-
+    //POST to create a new thought
+    router.route("/").get(getThoughts).post(createThought);
 
     //GET a single thought by ID
-
-
-    //POST to create a new thought
-
-
     // PUT to update thought by ID
-
-
     // DELETE thought by ID
-
-
-
+    router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
     
 
 //      /api/thoughts/:thoughtID/reactions
 
 
-
     //POST to create a reaction
-
-    
     //DELETE a reaction by reactions reactionID value
+router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
 
-    
+module.exports = router;
