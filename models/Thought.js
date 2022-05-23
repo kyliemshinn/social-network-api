6f6d9f753b048cc9357b3ae4eefc58e9a456736a
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-22 18:34:59 -0700

    add back in moment for created at section of thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require("mongoose");
+const moment = require('moment');
 
 const reactionSchema = new Schema(
   {
@@ -21,7 +22,7 @@ const reactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      // get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
   },
   {
@@ -41,7 +42,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      // get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     username: {
       type: String,
