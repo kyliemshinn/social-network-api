c9fabb74a73009d3db7d477eeb4ed3999e412256
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-22 17:56:50 -0700

    add connection file and take out mongodb route in server file

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-//required packages to use for program to run
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
+const cwd = process.cwd();
+
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-//middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
 });
-
-//log mongo queries
-mongoose.set('debug', true);
-
-//listen on port 3001
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
